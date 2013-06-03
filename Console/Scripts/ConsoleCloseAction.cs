54f1591889ddafca02f2cea1c8a0b3f47e2186c7
Mike Judge <mikelovesrobots@gmail.com>
2013-06-03 08:36:16 -0700

    Improve portability to Unity 3.5

--- Console/Scripts/ConsoleCloseAction.cs
+++ Console/Scripts/ConsoleCloseAction.cs
@@ -5,6 +5,10 @@ public class ConsoleCloseAction : ConsoleAction {
     public GameObject ConsoleGui;
 
     public override void Activate() {
+#if UNITY_3_5
+        ConsoleGui.active = false;
+#else
         ConsoleGui.SetActive(false);
+#endif
     }
 }
--- Console/Scripts/ConsoleOpenAction.cs
+++ Console/Scripts/ConsoleOpenAction.cs
@@ -5,6 +5,10 @@ public class ConsoleOpenAction : ConsoleAction {
     public GameObject ConsoleGui;
 
     public override void Activate() {
+#if UNITY_3_5
+        ConsoleGui.active = false;
+#else
         ConsoleGui.SetActive(true);
+#endif
     }
 }
