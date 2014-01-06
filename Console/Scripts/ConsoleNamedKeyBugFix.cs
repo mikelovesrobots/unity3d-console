54fac454f357586a8249341142460668aec9166e
Mike Judge <mikelovesrobots@gmail.com>
2014-01-06 11:42:57 -0800

    Bug fix for the classic dest.m_MultiFrameGUIState.m_NamedKeyControlList exception

---
+++ Console/Scripts/ConsoleNamedKeyBugFix.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConsoleNamedKeyBugFix : MonoBehaviour {
+    // Fixes the annoying issue described here: http://forum.unity3d.com/threads/158676-!dest-m_MultiFrameGUIState-m_NamedKeyControlList/page2
+    // seems to happen because ConsoleGUI creates and destroys a GUI textfield and the GUI doesn't know what to give focus to
+    void OnGUI() {
+        string controlName = gameObject.GetHashCode().ToString();
+        GUI.SetNextControlName(controlName);
+        Rect bounds = new Rect(0,0,0,0);
+        GUI.TextField(bounds, "", 0);
+    }
+}
